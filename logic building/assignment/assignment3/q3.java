bfeba7e19fdfa7d42dc24a5781f6b6460222b59f
Anuradha-Rodage <rodageanuradha@gmail.com>
2024-08-27 01:08:13 +0530

    Add files via upload

---
+++ logic building/assignment/assignment3/q3.java
@@ -0,0 +1,14 @@
+public class q3{
+
+    public static void main(String[] args) {
+      int i;
+       for (i = 1; i <= 100; i++) {
+          if ((i % 7 == 0) && (i > 0)) {
+          System.out.println("multiple of "+i);
+}
+
+} 
+}
+}
+  
+    
