5691a25c230edbd57b8381c4bc63377ee8d4fa67
Anuradha-Rodage <rodageanuradha@gmail.com>
2024-08-26 22:17:19 +0530

    Add files via upload

---
+++ logic building/assignment/assignment1/positive.java
@@ -0,0 +1,23 @@
+public class positive{
+ 
+  public static void main(String[] args) {
+ 
+    int number =45;
+ 
+     
+ 
+    if (number >= 0) {
+ 
+      System.out.println(number + " is positive.");
+ 
+ 
+    }
+ else {
+ 
+      System.out.println(number + "number is negative");
+ 
+    }
+ 
+  }
+ 
+}
