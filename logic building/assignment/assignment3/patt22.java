501ba539015a6c59920265e2c4b1080821540746
Anuradha-Rodage <rodageanuradha@gmail.com>
2024-08-27 00:16:18 +0530

    Add files via upload

---
+++ logic building/assignment/assignment3/patt22.java
@@ -0,0 +1,18 @@
+class patt22{
+public static void main(String args[])
+{
+for(int i=9;i<=1;i-=2)
+{
+for(int j=9;j>i;j--)
+{
+ System.out.print(" ");
+}
+for(int j=0;j<i;j++)
+{
+System.out.print("* ");
+
+}
+System.out.println("");
+}
+}
+}
