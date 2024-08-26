ca1b2c1d2f37b21d2a4fc701ceca7ecd64414a3e
Anuradha-Rodage <rodageanuradha@gmail.com>
2024-08-26 22:47:22 +0530

    Add files via upload

---
+++ logic building/assignment/assignment2/switch.java
@@ -0,0 +1,20 @@
+class switch{
+ public static void main(String args[]){
+      int val=5;
+switch(val){
+case 1:
+     System.out.println("today is a monday");
+ break;
+case 2:
+     System.out.println("today is tuesday");
+break;
+
+case 3:
+    System.out.println("today is wednesday");
+break;
+default:
+ System.out.println("invalid choice");
+break;
+}
+}
+}
