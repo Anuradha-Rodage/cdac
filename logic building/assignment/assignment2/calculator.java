bf8f2ad3c1f24eda21fddba1a5080ba0ea0f1d2b
Anuradha-Rodage <rodageanuradha@gmail.com>
2024-08-26 23:05:13 +0530

    Add files via upload

---
+++ logic building/assignment/assignment2/calculator.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+class calculator {
+  public static void main(String[] args) {
+
+    char operator;
+    Double number1, number2, result;
+
+   
+    Scanner input = new Scanner(System.in);
+
+    
+    System.out.println("Choose an operator: +, -, *, or /");
+    operator = input.next().charAt(0);
+
+    
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+    switch (operator) {
+
+     
+      case '+':
+        result = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + result);
+        break;
+
+     
+      case '-':
+        result = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + result);
+        break;
+
+      
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+        break;
+
+      
+      case '/':
+        if(number2!=0)
+        {
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        }
+        else
+        {
+         System.out.println("error: we can not devide by zero");
+
+         }
+        break;
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+
+    input.close();
+  }
+}
