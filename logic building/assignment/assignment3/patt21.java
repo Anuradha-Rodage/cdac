1f07434cdbd0fc3cca6c86b2fa15ebff0c9da618
Anuradha-Rodage <rodageanuradha@gmail.com>
2024-08-27 00:14:41 +0530

    Add files via upload

---
+++ logic building/assignment/assignment3/patt21.java
@@ -0,0 +1,17 @@
+class patt21{
+public static void main(String args[])
+{
+for(int i=1;i<=10;i+=2)
+{
+for(int j=1;j<=i;j+=2)
+{
+System.out.print(j);
+if(j<i)
+{
+System.out.print("*");
+}
+}
+System.out.println(" ");
+}
+}
+}
